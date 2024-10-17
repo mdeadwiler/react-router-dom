9a7bd0a64e786d3a2da8555820a9b163b89f932e
MDeadwiler <mdeadwiler@email.com>
2024-10-17 11:32:33 -0400

    added-links-routerDom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import { useState } from 'react';
 import PokemonList from './components/PokemonList';
+import NavBar from './components/NavBar';
 
 const initialState = [
   { _id: 1, name: 'bulbasaur', weight: 69, height: 7 },
@@ -17,6 +18,7 @@ const App = () => {
 
   return (
     <>
+      <NavBar />
       <h1>Pokemon!</h1>
       <PokemonList pokemon={pokemon} />
     </>
