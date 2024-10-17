c021bf97cbacc4172505ddc3501cc960dc426d7e
MDeadwiler <mdeadwiler@email.com>
2024-10-17 14:19:18 -0400

    added-formData

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
 // src/App.jsx
-import { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+// src/App.jsx
+import PokemonForm from "./components/PokemonForm";
 
-
-import PokemonList from './components/PokemonList';
-import NavBar from './components/NavBar';
-import PokemonDetails from './components/PokemonDetails';
+import PokemonList from "./components/PokemonList";
+import NavBar from "./components/NavBar";
+import PokemonDetails from "./components/PokemonDetails";
 
 const initialState = [
-  { _id: 1, name: 'bulbasaur', weight: 69, height: 7 },
-  { _id: 2, name: 'ivysaur', weight: 130, height: 10 },
-  { _id: 3, name: 'venusaur', weight: 1000, height: 20 },
-  { _id: 4, name: 'charmander', weight: 85, height: 6 },
-  { _id: 5, name: 'charmeleon', weight: 190, height: 11 },
-  { _id: 5, name: 'marquise', weight: 200, height: 11 },
+  { _id: 1, name: "bulbasaur", weight: 69, height: 7 },
+  { _id: 2, name: "ivysaur", weight: 130, height: 10 },
+  { _id: 3, name: "venusaur", weight: 1000, height: 20 },
+  { _id: 4, name: "charmander", weight: 85, height: 6 },
+  { _id: 5, name: "charmeleon", weight: 190, height: 11 },
+  { _id: 5, name: "marquise", weight: 200, height: 11 },
 ];
 
 const App = () => {
   const [pokemon, setPokemon] = useState(initialState);
 
+  const addPokemon = (newPokemonData) => {
+    newPokemonData._id = pokemon.length + 1;
+    setPokemon([...pokemon, newPokemonData]);
+  };
 
   return (
     <>
-      
       <h1>Pokemon!</h1>
       <NavBar />
       <Routes>
-      <Route path="/" element={<h2>Home Page</h2>} />
-      <Route path="/pokemon" element={<PokemonList pokemon={pokemon} />} /> 
-      <Route path="/pokemon/:pokemonId" element={<PokemonDetails pokemon={pokemon} />} />
-      <Route path="*" element={<h2>Whoops, nothing here!</h2>} />  
+        <Route path="/" element={<h2>Home Page</h2>} />
+        <Route path="/pokemon" element={<PokemonList pokemon={pokemon} />} />
+        <Route
+          path="/pokemon/:pokemonId"
+          element={<PokemonDetails pokemon={pokemon} />}
+        />
+        <Route
+          path="/pokemon/new"
+          element={<PokemonForm addPokemon={addPokemon} />}
+        />
+        <Route path="*" element={<h2>Whoops, nothing here!</h2>} />
       </Routes>
     </>
   );
