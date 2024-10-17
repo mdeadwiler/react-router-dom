2737e76492d6aefb564199490375d01b324d75cd
MDeadwiler <mdeadwiler@email.com>
2024-10-17 14:32:15 -0400

    added-formData-with-redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // src/App.jsx
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-// src/App.jsx
 import PokemonForm from "./components/PokemonForm";
 
 import PokemonList from "./components/PokemonList";
